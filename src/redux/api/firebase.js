309d8d0752fbb8c9bc8715d6b2fd9ac40bd12692
Kamal kishore joshi <mr.joshitech@gmail.com>
2021-10-15 21:59:48 +0530

    rmid

--- src/redux/api/firebase.js
+++ src/redux/api/firebase.js
@@ -5,13 +5,7 @@ import "firebase/storage";
 import "firebase/functions";
 
 const config = {
-  apiKey: "AIzaSyD9703msQFncIRY6nu5gcoVEuy6j_wJ6vg",
-  authDomain: "social-app-884b2.firebaseapp.com",
-  projectId: "social-app-884b2",
-  storageBucket: "social-app-884b2.appspot.com",
-  messagingSenderId: "838513615558",
-  appId: "1:838513615558:web:bf8f669922adcfb97eedc7",
-  measurementId: "G-ZCYR957KRZ",
+  
 };
 
 if (!firebase.apps.length) {
