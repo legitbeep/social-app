1801795dc7de6f50e8b666a177765aaa38c6e300
Kamal joshi <56737320+mr-joshi@users.noreply.github.com>
2021-10-19 11:10:22 +0530

    profile (#4)
    * profile
    
    * Profile DOne
    
    Co-authored-by: Kamal kishore joshi <mr.joshitech@gmail.com>

--- src/Views/profile/Profile.js
+++ src/Views/profile/Profile.js
@@ -1,14 +1,25 @@
-import React, { Fragment } from 'react'
-import ProfileDetails from '../../components/profile/ProfileDetails'
-import Profileimages from '../../components/profile/Profileimages'
-
+import React from 'react'
+import ProfileLayout from '../../layout/ProfileLayout'
+import {Switch, Route,useRouteMatch } from 'react-router-dom'
+import Posts from './overview/Posts'
 const Profile = () => {
+const {path}=useRouteMatch();
 
     return (
-        <Fragment>
-        <Profileimages/> 
-        <ProfileDetails/>
-        </Fragment>
+        <>
+            <ProfileLayout>
+                <Switch>
+                <Route path={`${path}`} exact><Posts/></Route>
+                <Route path={`${path}/followers`} exact></Route>
+                <Route path={`${path}/following`} exact></Route>
+                <Route path={`${path}/saved-posts`} exact></Route>
+                <Route path={`${path}/followers`} exact></Route>
+
+                <Route path="*" exact>NotFOUND </Route>
+                </Switch>
+            </ProfileLayout>
+        
+        </>
     )
 }
 
---
+++ src/components/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const blog = () => {
+    return (
+        <div>
+            adasdsadasdasd
+        </div>
+    )
+}
+
+export default blog
