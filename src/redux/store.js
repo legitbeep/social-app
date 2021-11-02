b7fe183744b0f13312b6f90f6002456aa15a15bb
Kamal joshi <56737320+mr-joshi@users.noreply.github.com>
2021-11-02 12:04:40 +0530

    Update create blog (#7)
    * profile
    
    * Profile DOne
    
    * Home Page Done
    
    * post,create post
    
    * some changes in postpage
    
    * UI corrections
    
    * updation of create post
    
    * firbase sahi kr
    
    Co-authored-by: Kamal kishore joshi <mr.joshitech@gmail.com>
    Co-authored-by: Harsh Yadav <50167891+legitbeep@users.noreply.github.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice";
+import {authApi} from './api/auth'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    
   },
 });
